fcd8953eee77be04d67c23c04d231ea45d85a2fa
fasterpleas <121158591+fasterpleas@users.noreply.github.com>
2022-12-24 14:15:32 +0530

    Delete check.c

--- check.c
+++
@@ -1,7 +0,0 @@
-// CHECKING A PRORAM
-                             #include<stdio.h>
-                                 int main()
-	                     {
-			       printf("HELLO DARLING\n");
-			        return 0;
-			     }
