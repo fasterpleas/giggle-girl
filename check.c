1caef65f0686e93a70b8f108d2779688d89c8ebf
fasterpleas <121158591+fasterpleas@users.noreply.github.com>
2022-12-24 14:10:59 +0530

    Add files via upload

---
+++ check.c
@@ -0,0 +1,7 @@
+// CHECKING A PRORAM
+                             #include<stdio.h>
+                                 int main()
+	                     {
+			       printf("HELLO DARLING\n");
+			        return 0;
+			     }
